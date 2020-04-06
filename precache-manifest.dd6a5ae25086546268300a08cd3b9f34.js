7256351c84feb47059b1cda578c0fb7765b07e7a
Maksym PC home <malishev.maksim@gmail.com>
2020-04-06 17:12:40 +0300

    Revert "Trying to fix img problem"
    This reverts commit 92aa2dbc276bd65bd34bd9bcdd566a193ff22c63.

---
+++ precache-manifest.dd6a5ae25086546268300a08cd3b9f34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cd1788a208d645b2d9e90624fbb1af2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "da19c91d2347e5c50ced",
+    "url": "./static/css/2.a33d7f97.chunk.css"
+  },
+  {
+    "revision": "7a134f38157eb0bee823",
+    "url": "./static/css/main.30db98d1.chunk.css"
+  },
+  {
+    "revision": "da19c91d2347e5c50ced",
+    "url": "./static/js/2.695413be.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.695413be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a134f38157eb0bee823",
+    "url": "./static/js/main.4d7befe9.chunk.js"
+  },
+  {
+    "revision": "b51f6897157dba573fc3",
+    "url": "./static/js/runtime-main.4bf1e49a.js"
+  }
+]);
