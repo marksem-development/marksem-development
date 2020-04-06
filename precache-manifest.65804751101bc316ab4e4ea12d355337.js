63b76688619e9c2b7b5037592cda2d85aeb734a3
marksem-development <marksem.development@gmail.com>
2020-04-06 13:55:42 +0300

    Fixing the route error

---
+++ precache-manifest.65804751101bc316ab4e4ea12d355337.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645b30de01ccc5e8a944c737f555f427",
+    "url": "./index.html"
+  },
+  {
+    "revision": "da19c91d2347e5c50ced",
+    "url": "./static/css/2.a33d7f97.chunk.css"
+  },
+  {
+    "revision": "523b30b2da495a1eefd8",
+    "url": "./static/css/main.30db98d1.chunk.css"
+  },
+  {
+    "revision": "da19c91d2347e5c50ced",
+    "url": "./static/js/2.695413be.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.695413be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "523b30b2da495a1eefd8",
+    "url": "./static/js/main.b45c1faf.chunk.js"
+  },
+  {
+    "revision": "b51f6897157dba573fc3",
+    "url": "./static/js/runtime-main.4bf1e49a.js"
+  }
+]);
