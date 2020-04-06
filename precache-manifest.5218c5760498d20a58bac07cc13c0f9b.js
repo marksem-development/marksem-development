1bf72f25ad9d33ba352cfa97a838b5d829fa4a82
Alexander Shmyrkov <alexshmyrkov@Alexander-Shmyrkovs-MacBook-Pro.local>
2020-04-06 13:22:16 +0300

    Initial commit -main page

---
+++ precache-manifest.5218c5760498d20a58bac07cc13c0f9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac76461679faebb9449113bd07bb95f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da19c91d2347e5c50ced",
+    "url": "/static/css/2.a33d7f97.chunk.css"
+  },
+  {
+    "revision": "0aaf4e467d3f8ccf4087",
+    "url": "/static/css/main.30db98d1.chunk.css"
+  },
+  {
+    "revision": "da19c91d2347e5c50ced",
+    "url": "/static/js/2.695413be.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.695413be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aaf4e467d3f8ccf4087",
+    "url": "/static/js/main.6364dca1.chunk.js"
+  },
+  {
+    "revision": "482659de96e4fa5d6cd7",
+    "url": "/static/js/runtime-main.77c57556.js"
+  }
+]);
