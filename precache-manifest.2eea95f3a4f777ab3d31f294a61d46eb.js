92aa2dbc276bd65bd34bd9bcdd566a193ff22c63
Maksym PC home <malishev.maksim@gmail.com>
2020-04-06 16:58:28 +0300

    Trying to fix img problem

---
+++ precache-manifest.2eea95f3a4f777ab3d31f294a61d46eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cfbdeaf2ba5f4a0b82ad2b9bfb4cbaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da19c91d2347e5c50ced",
+    "url": "/static/css/2.a33d7f97.chunk.css"
+  },
+  {
+    "revision": "702082d4f389a2379036",
+    "url": "/static/css/main.971c94a2.chunk.css"
+  },
+  {
+    "revision": "da19c91d2347e5c50ced",
+    "url": "/static/js/2.695413be.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.695413be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "702082d4f389a2379036",
+    "url": "/static/js/main.6d152e84.chunk.js"
+  },
+  {
+    "revision": "482659de96e4fa5d6cd7",
+    "url": "/static/js/runtime-main.77c57556.js"
+  }
+]);
